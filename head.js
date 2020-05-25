db1c70ace30a3dff421c8ee1984434634f190727
fatimasajadi <s.sfatemeh@gmail.com>
2020-05-25 20:24:13 +0000

    using assertequal function to test if the head function is returning the first item of the array

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+
+  const acceptMsg = `✅  Assertion Passed:  ${actual}  ===  ${expected}`;
+  const errorMsg = `🛑  Assertion Failed:  ${actual}  !==  ${expected}`;
+
+  console.log(actual === expected ? acceptMsg : errorMsg);
+};
+
+
+let myArr = [];
+
+function head(myArr) {
+  if (myArr.length == 0) {
+    return undefined;
+  }
+
+  const myHead = myArr[0];
+  return myHead;
+}
+
+assertEqual(head([]), 5);
