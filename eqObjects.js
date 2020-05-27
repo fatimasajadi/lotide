c89b38bfd354e856e8edb5c3bbe8fe3dc475a75c
fatimasajadi <s.sfatemeh@gmail.com>
2020-05-27 20:44:40 +0000

    eqObject assertion

---
+++ eqObjects.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+
+  const acceptMsg = `✅  Assertion Passed:  ${actual}  ===  ${expected}`;
+  const errorMsg = `🛑  Assertion Failed:  ${actual}  !==  ${expected}`;
+
+
+  console.log(eqObjects(actual, expected) ? acceptMsg : errorMsg);
+};
+
+
+const eqObjects = function(o1, o2) {
+  for (let p in o1) {
+    if (o1.hasOwnProperty(p)) {
+      if (o1[p] !== o2[p]) {
+        return false;
+      }
+    }
+  }
+  for (let p in o2) {
+    if (o2.hasOwnProperty(p)) {
+      if (o1[p] !== o2[p]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === arr2) return true;
+  if (arr1 === null || arr2 === null) return false;
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; ++i) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd = { c: "1", d: ["2", 3] };
+console.log(eqObjects(cd, cd2)); // => false
