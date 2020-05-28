018f0d64a97773613e32e3f10767200abdd38932
fatimasajadi <s.sfatemeh@gmail.com>
2020-05-28 18:21:36 +0000

    A function similar to findKey function in JS

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+
+  const acceptMsg = `✅  Assertion Passed:  ${actual}  ===  ${expected}`;
+  const errorMsg = `🛑  Assertion Failed:  ${actual}  !==  ${expected}`;
+
+  console.log(actual === expected ? acceptMsg : errorMsg);
+};
+let obj1 = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+const findKey = function(obj, callback) {
+  let str = "";
+  for (let item in obj) {
+    if (callback(obj[item])) {
+      str = item;
+      break;
+    }
+  }
+  return str;
+};
+
+
+let result1 = findKey(obj1, x => x.stars === 2);
+console.log(result1);
+
+//Test
+assertEqual(findKey(obj1, x => x.stars === 2), "noma");
