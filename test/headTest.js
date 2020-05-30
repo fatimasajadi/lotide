5904682441b73f5536004585419780d10b3c35c4
fatimasajadi <s.sfatemeh@gmail.com>
2020-05-30 23:58:52 +0000

    seprating test from its function

---
+++ test/headTest.js
@@ -0,0 +1,4 @@
+const head = require('../head');
+const assertEqual = require('../assertEqual');
+
+assertEqual(head([2]), 2);
---
+++ test/tailTest.js
@@ -0,0 +1,5 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+let myArr = ["banana", "apple", "orange"];
+assertEqual(myArr.length, 3);
