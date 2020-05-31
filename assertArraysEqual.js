be64e632667ac352795135b81ba6ea6e5278e4de
fatimasajadi <s.sfatemeh@gmail.com>
2020-05-31 00:17:06 +0000

    add eqArrays module to assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,9 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   const acceptMsg = `✅  Assertion Passed:  ${actual}  ===  ${expected}`;
   const errorMsg = `🛑  Assertion Failed:  ${actual}  !==  ${expected}`;
 
 
   console.log(eqArrays(actual, expected) ? acceptMsg : errorMsg);
-};
-
-
-
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1 === arr2) return true;
-  if (arr1 === null || arr2 === null) return false;
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; ++i) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
 };
